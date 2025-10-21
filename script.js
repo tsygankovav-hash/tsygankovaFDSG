849b2835d136c905b360fe0bc16ebe87b516a0aa
tsygankovav-hash <tsygankovavictoriya@gmail.com>
2025-10-21 14:13:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+ document.addEventListener('DOMContentLoaded', () => {
+  const learnMoreBtn = document.getElementById('learnMoreBtn');
+  learnMoreBtn?.addEventListener('click', () => {
+    alert("Google — мировой лидер в сфере интернет-технологий!");
+  });
+});
